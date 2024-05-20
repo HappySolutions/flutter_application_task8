df275c836cdba0736d97fe84e28150cc39f72cf4
HappySolutions <heba.odesk.tasks@gmail.com>
2024-05-20 13:41:32 +0300

    add feilds

--- lib/pages/form_page.dart
+++ lib/pages/form_page.dart
@@ -7,44 +7,52 @@ class FormPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.orange,
       appBar: AppBar(
-        title: const Text('Shipping information'),
+        title: const Text('Sign Up Form'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              MyTextFeild(
-                name: 'Email',
-                maxLength: 50,
-                obsecureText: false,
-                keyboardType: TextInputType.text,
-                floatinLableColor: Colors.orange,
-              ),
-              MyTextFeild(
-                name: 'Username',
-                maxLength: 50,
-                obsecureText: false,
-                keyboardType: TextInputType.text,
-                floatinLableColor: Colors.black12,
-              ),
-              MyTextFeild(
-                name: 'Password',
-                maxLength: 50,
-                obsecureText: true,
-                keyboardType: TextInputType.text,
-                floatinLableColor: Colors.black26,
-              ),
-              MyTextFeild(
-                name: 'Confirm Password',
-                maxLength: 50,
-                obsecureText: true,
-                keyboardType: TextInputType.text,
-                floatinLableColor: Colors.black38,
-              ),
-              ElevatedButton(onPressed: () {}, child: Text('Sign Up'))
-            ],
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+            ),
+            child: Column(
+              children: [
+                MyTextFeild(
+                  name: 'Email',
+                  maxLength: 50,
+                  obsecureText: false,
+                  keyboardType: TextInputType.text,
+                  floatinLableColor: Colors.orange,
+                ),
+                MyTextFeild(
+                  name: 'Username',
+                  maxLength: 50,
+                  obsecureText: false,
+                  keyboardType: TextInputType.text,
+                  floatinLableColor: Colors.black12,
+                ),
+                MyTextFeild(
+                  name: 'Password',
+                  maxLength: 50,
+                  obsecureText: true,
+                  keyboardType: TextInputType.text,
+                  floatinLableColor: Colors.black26,
+                ),
+                MyTextFeild(
+                  name: 'Confirm Password',
+                  maxLength: 50,
+                  obsecureText: true,
+                  keyboardType: TextInputType.text,
+                  floatinLableColor: Colors.black38,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text('Sign Up'))
+              ],
+            ),
           ),
         ),
       ),
